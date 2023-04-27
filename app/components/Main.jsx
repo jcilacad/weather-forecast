ae58083b1e9fa4c889ac38e8283ded28601437b7
jcilacad <johnchristopherilacad27@gmail.com>
2023-04-27 22:41:05 +0800

    [UPDATE] Footer

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -15,10 +15,13 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link
+        color="inherit"
+        href="https://weather-forecast-three-xi.vercel.app/"
+      >
+        Weather Forecast
       </Link>{" "}
-      {new Date().getFullYear()}
+      2023
       {"."}
     </Typography>
   );
@@ -83,17 +86,6 @@ export default function Album() {
       </main>
       {/* Footer */}
       <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
         <Copyright />
       </Box>
       {/* End footer */}
